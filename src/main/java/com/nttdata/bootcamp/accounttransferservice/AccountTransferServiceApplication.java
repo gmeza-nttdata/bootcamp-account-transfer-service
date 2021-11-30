7c34653621cece3b531a64c47f45cda2719a8ecc
gmeza-nttdata <94468068+gmeza-nttdata@users.noreply.github.com>
2021-11-30 01:56:54 -0500

    Initial commit

---
+++ src/main/java/com/nttdata/bootcamp/accounttransferservice/AccountTransferServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nttdata.bootcamp.accounttransferservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountTransferServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountTransferServiceApplication.class, args);
+	}
+
+}
